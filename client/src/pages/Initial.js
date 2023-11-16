e72016edf903a2786e6af33feef0e1a190f2f7e4
Sandro Guedes <sandroguedes_9@hotmail.com>
2023-11-15 23:21:35 -0300

    Initial page
    update the inicial page

--- client/src/pages/Initial.js
+++ client/src/pages/Initial.js
@@ -1,55 +1,103 @@
-import React from "react"
+import React, { useState } from 'react';
 import { Link } from "react-router-dom"
 import "../styles/Initial.css"
-import desktop from "../images/área.png"
-import notes from "../images/anotação.png"
-import gadgets from "../images/calculadora.png"
-import message from "../images/mensagem.png"
+import LogoImg from '../images/mclivrinhu.png';
+import BannerImg from '../images/asasas.png';
+import Imagem2 from '../images/image2.png'
+import Imagem1 from '../images/201560.png'
+import Imagem3 from '../images/imagenoti3.png'
+import Imagem4 from '../images/1827402.png'
 import { NaoLogado } from "../components/IsLogged"
+import { FiAlertOctagon } from "react-icons/fi";
 
 const Initial = () => {
 
-    function Functionality (props) {
-        return (
-            <div className="functionalities__content">
-                <div className="functionalities__image">
-                    <img src={props.image} alt=""/>
-                </div>
-                <div className="functionalities__description">
-                    <p>{props.description}</p>
-                </div>
-            </div>
-        )
-    }
 
-    return (
-        <div>
-            {NaoLogado()}
-            <div className="main">
-                <div className="main__title">
-                    <h1>Potencialize sua produtividade acadêmica com o Focus Task!</h1>
-                </div>
-                <div className="main__description">
-                    <h3>Simplifique sua rotina acadêmica, elimine a papelada e concentre-se no que realmente importa: seus estudos.</h3>
-                </div>
-                <div className="main__button">
-                    <button><h4>Começe já! →</h4></button>
-                    <div className="main__free">
-                        <p>Teste grátis ✦ Não é necessário cartão de crédito</p>
-                    </div>
-                </div>
-            </div> 
-            <div className="space"></div>
-            <div className="functionalities">
-                <div className="functionalities__title">
-                    <h1>Funcionalidades que o nosso sistema oferece:</h1>
-                </div>
-                <Functionality image={desktop} description="Crie áreas de trabalho personalizadas para cada disciplina, projeto ou tarefa. Mantenha seus materiais e tarefas organizados em um espaço dedicado a cada contexto acadêmico."/>
-                <Functionality image={notes} description="Faça anotações diretamente no sistema. Registre ideias, insights, lembretes e informações importantes relacionadas a projetos e disciplinas específicas. Tenha tudo em um só lugar para fácil acesso."/>
-                <Functionality image={message} description="Receba notificações em tempo real sobre atividades relevantes nos seus quadros. Seja informado sobre comentários, atualizações e prazos. Fique sempre atualizado e não perca nenhum detalhe importante."/>
-                <Functionality image={gadgets} description="Acesse gadgets úteis, como timers e calculadoras, diretamente no sistema. Otimize seu tempo de estudo, faça cálculos rápidos e tenha ferramentas convenientes ao seu alcance."/>
-            </div>
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
+    return ( 
+        <section>
+        <div className="circle"></div>
+        <div className="content">
+          <div className="text">
+            <h2>Focus <br /><span>Task</span></h2>
+            <p>FocusTask é uma aplicação que, com suas funcionalidades, será capaz de auxiliar estudantes a organizarem sua vida acadêmica com a vida pessoal de forma harmônica com suas incríveis funcionalidades </p>
+            <p className='p1'><span><FiAlertOctagon/> Teste grátis <FiAlertOctagon/></span> Não é necessário cartão de crédito.</p>
+            <button onClick={openModal}>Ver funcionalidades</button>
+          </div>
         </div>
+  
+        <div className="boxImg">
+          <img src={BannerImg} alt="" className="img1" />
+        </div>
+  
+        {modalOpen && (
+  <div className="__modal">
+    <div className="__modal-content">
+      <div className="__modal-header">
+        <h3>Funcionalidades oferecidas pelo sistema:</h3>
+      </div>
+      <div className="__slash-1"></div>
+      <div className="__modal-body">
+        <div className="__card">
+          <div className="__header">
+            <div className="__image">
+              <img src={Imagem1} alt="" />
+            </div>
+          </div>
+          <div className="__info">
+            <p className="__description">Crie áreas de trabalho personalizadas para cada disciplina, projeto ou tarefa. Mantenha seus materiais e tarefas organizados em um espaço dedicado a cada contexto acadêmico.</p>
+          </div>
+        </div>
+
+        <div className="__card">
+          <div className="__header">
+            <div className="__image">
+              <img src={Imagem2} alt="" />
+            </div>
+          </div>
+          <div className="__info">
+            <p className="__description">Faça anotações diretamente no sistema. Registre ideias, insights, lembretes e informações importantes relacionadas a projetos e disciplinas específicas. Tenha tudo em um só lugar para fácil acesso.</p>
+          </div>
+        </div>
+
+        <div className="__card">
+          <div className="__header">
+            <div className="__image">
+              <img src={Imagem3} alt="" />
+            </div>
+          </div>
+          <div className="__info">
+            <p className="__description">Receba notificações em tempo real sobre atividades relevantes nos seus quadros. Seja informado sobre comentários, atualizações e prazos. Fique sempre atualizado e não perca nenhum detalhe importante.</p>
+          </div>
+        </div>
+
+        <div className="__card">
+          <div className="__header">
+            <div className="__image">
+              <img src={Imagem4} alt="" />
+            </div>
+          </div>
+          <div className="__info">
+            <p className="__description">Acesse gadgets úteis, como timers e calculadoras, diretamente no sistema. Otimize seu tempo de estudo, faça cálculos rápidos e tenha ferramentas convenientes ao seu alcance.</p>
+          </div>
+        </div>
+      </div>
+      <div className="__slash-1"></div>
+      <button className='__btnfechar' onClick={closeModal}>Fechar</button>
+    </div>
+  </div>
+)}
+
+      </section>
     )
 }
 
